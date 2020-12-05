b9c832e91ed515096693889939d6f48f6bca9dbe
Ryo Ando <sasigume@Ryos-MacBook-Pro.local>
2020-12-06 03:41:11 +0900

    Updated styles

--- components/intro.js
+++ components/intro.js
@@ -2,13 +2,35 @@ import {CONST_REPO_URL, CONST_SITE_URL, CONST_SITE_NAME, CONST_TWITTER_URL, CONS
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <nav className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         {CONST_SITE_NAME}
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Hello.
-      </h4>
-    </section>
+      <p className="text-center md:text-left text-lg mt-5 md:pl-8">
+          I'm a student.<br />
+          I have a dream: the future where everyone has equal rights to learn anytime, anywhere.
+      </p>
+      <div className="flex flex-col">
+          <a href={CONST_REPO_URL} className="p-card">
+            <h3>Repository &rarr;</h3>
+            <p>You can view the source code.</p>
+          </a>
+
+          <a href={CONST_YOUTUBE_URL} className="p-card">
+            <h3>YouTube &rarr;</h3>
+            <p>Watch my videos on YouTube.</p>
+          </a>
+
+          <a href={CONST_TWITTER_URL} className="p-card">
+            <h3>Twitter &rarr;</h3>
+            <p>Feel free to follow me on Twitter.</p>
+          </a>
+
+          <a href={CONST_LAPRAS_URL} className="p-card">
+            <h3>LAPRAS &rarr;</h3>
+            <p>For more info about me, visit my page onn LAPRAS.</p>
+          </a>
+        </div>
+    </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -18,40 +18,7 @@ export default function Index({ preview, allPosts }) {
       </Head>
 
       <Container>
-      <main className="p-main">
-        <h1 className="p-title">
-          Welcome to <a href={CONST_SITE_URL}>{CONST_SITE_NAME}</a>
-        </h1>
-
-        <p className="p-description">
-          I'm a student.<br />
-          I have a dream: the future where everyone has equal rights to learn anytime, anywhere.
-        </p>
-
-        <div className="p-grid">
-          <a href={CONST_REPO_URL} className="p-card w-full">
-            <h3>Repository &rarr;</h3>
-            <p>You can view the source code.</p>
-          </a>
-
-          <a href={CONST_YOUTUBE_URL} className="p-card w-full">
-            <h3>YouTube &rarr;</h3>
-            <p>Watch my videos on YouTube.</p>
-          </a>
-
-          <a href={CONST_TWITTER_URL} className="p-card w-full">
-            <h3>Twitter &rarr;</h3>
-            <p>Feel free to follow me on Twitter.</p>
-          </a>
-
-          <a href={CONST_LAPRAS_URL} className="p-card w-full">
-            <h3>LAPRAS &rarr;</h3>
-            <p>For more info about me, visit my page onn LAPRAS.</p>
-          </a>
-        </div>
-      </main>
-
-      <Intro />
+      <Intro />      
           {heroPost && (
             <HeroPost
               title={heroPost.title}
@@ -63,17 +30,6 @@ export default function Index({ preview, allPosts }) {
             />
           )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-
-      <footer className="p-footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="p-logo" />
-        </a>
-      </footer>
         </Container>
     </Layout>
   )
