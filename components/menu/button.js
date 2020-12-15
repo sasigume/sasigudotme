3535e93013ce9d87e78e0020be8391bc6bb50fb1
Ryo Ando <sasigume@gmail.com>
2020-12-16 03:26:39 +0900

    menu target is now _blank

--- components/menu/button.js
+++ components/menu/button.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Button({path, label, iconStyle, iconName}) {
     return(
     <Link href={path}>
-      <a className="inline-block mr-0 md:mr-4 last:mr-0 my-1 md:my-4 no-underline py-3 px-4 border-2 transition duration-300 border-gray-100 hover:border-gray-500 rounded-lg">
+      <a  target="_blank" className="inline-block mr-0 md:mr-4 last:mr-0 my-1 md:my-4 no-underline py-3 px-4 border-2 transition duration-300 border-gray-100 hover:border-gray-500 rounded-lg">
         <b className="block font-bold text-xl">
           <FontAwesomeIcon className="w-5 mr-2 mb-1 inline" icon={[iconStyle, iconName]}/>{label}</b>
       </a>
