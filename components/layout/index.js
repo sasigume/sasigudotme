2ed944c62c1e14cc0acf2576d7fd11ac38e362f9
Ryo Ando <sasigume@gmail.com>
2020-12-16 17:37:47 +0900

    moved SVG into layout

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,8 +1,7 @@
 import Footer from './footer'
 import Meta from './meta'
 import SideMenu from './sideMenu'
-import {CONST_SITE_NAME, CONST_LOGO_SVG} from '@/libs/constants'
-
+import {CONST_SITE_NAME,} from '@/libs/constants'
 import cn from 'classnames'
 
 export default function Layout({ preview, children, isHome}) {
@@ -20,7 +19,9 @@ export default function Layout({ preview, children, isHome}) {
         {
           "w-40 mb-4": isHome,
           "w-20": !isHome
-        })}>{CONST_LOGO_SVG}</div>
+        })}>
+          <svg className="hover:animate-spin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 275 294"><path fill="#007ca8" d="M149.76 118.3l-48.32-45.06L24.32 0l.2 92.09 76.92 70.48 101.44 92.93"/><path fill="#00f598" d="M148.46 205.08L170.09 92.2V0l-68.65 73.24V294"/><path fill="#ff8300" d="M148.46 205.08L275 92.09l-104.91.11-68.65 70.37L0 255.5"/></svg>
+        </div>
           <h1 className="text-4xl uppercase tracking-widest italic">{CONST_SITE_NAME}</h1>
         </div>
         <aside className="">
