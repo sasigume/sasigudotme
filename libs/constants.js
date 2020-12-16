9b1b46d3f9a45ce8e186b5a3d2f3cc77044b9969
Ryo Ando <sasigume@gmail.com>
2020-12-16 11:19:28 +0900

    added Link list to constants

--- libs/constants.js
+++ libs/constants.js
@@ -1,5 +1,5 @@
 export const CONST_MYNAME = "sasigume"
-export const CONST_SITE_NAME = CONST_MYNAME + "'s Works"
+export const CONST_SITE_NAME = "Hello, I'm " + CONST_MYNAME
 export const CONST_SITE_URL = "https://sasigu.me"
 export const CONST_BLOG_URL = "https://blog.sasigu.me"
 export const CONST_REPO_URL = 'https://github.com/sasigume/sasigudotme'
@@ -8,3 +8,57 @@ export const CONST_FB_URL = 'https://facebook.com/sasigume'
 export const CONST_YOUTUBE_URL = 'https://www.youtube.com/channel/UCzOySm9dwHiu0d-bWIaInRA'
 export const CONST_LAPRAS_URL = 'https://lapras.com/public/NG9RFWQ'
 export const CONST_OG_IMAGE_URL = 'https://og-image.now.sh/Welcome%20to%20**Sasigu.me**.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg&images=https%3A%2F%2Fd21buns5ku92am.cloudfront.net%2F41748%2Fimages%2F347966-contentful-logo-wordmark-dark%2520%25281%2529-4cd185-large-1582664935.png&widths=undefined&widths=250&heights=undefined&heights=50'
+
+export const CONST_SKILLS = [
+    {
+      label: "WordPress",
+      iconStyle: "fab",
+      iconName: "wordpress",
+      level: 2
+    },
+    {
+      label: "HTML5",
+      iconStyle: "fab",
+      iconName: "html5",
+      level: 3
+    },
+    {
+      label: "CSS3",
+      iconStyle: "fab",
+      iconName: "html5",
+      level: 3
+    },
+    {
+        label: "この色が「初心者」",
+        iconStyle: "fas",
+        iconName: "dizzy",
+        level: 1
+      },
+]
+
+export const CONST_LINKS = [
+    {
+      label: "Facebook",
+      iconStyle: "fab",
+      iconName: "facebook",
+      path: CONST_FB_URL,
+    },
+    {
+      label: "YouTube",
+      iconStyle: "fab",
+      iconName: "youtube",
+      path: CONST_YOUTUBE_URL,
+    },
+    {
+      label: "Twitter",
+      iconStyle: "fab",
+      iconName: "twitter",
+      path: CONST_TWITTER_URL,
+    },
+    {
+      label: "LAPRAS",
+      iconStyle: "fas",
+      iconName: "external-link-alt",
+      path: CONST_LAPRAS_URL,
+    }
+  ]
