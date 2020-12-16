866163706c410f01d857bb5ee19088100aa568fe
Ryo Ando <sasigume@gmail.com>
2020-12-16 11:19:03 +0900

    added copyright to footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,25 +1,20 @@
 import Container from '@/components/container'
-import {CONST_REPO_URL, CONST_TWITTER_URL} from '@/libs/constants'
+import {CONST_REPO_URL} from '@/libs/constants'
 
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="mt-auto bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-20 flex flex-col items-center">
           <div className="flex flex-col md:flex-row justify-center items-center">
-            <a
-              href={CONST_TWITTER_URL}
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 md:px-8 duration-200 transition-colors mb-6 md:mb-0"
-            >
-              Follow me on Twitter
-            </a>
             <a
               href={CONST_REPO_URL}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
             </a>
+            <small className="mx-3">&copy; Ryo Ando 2020</small>
           </div>
         </div>
       </Container>
