79af72173f098db03dc295f87123a19b18cc465a
Ryo Ando <sasigume@gmail.com>
2020-12-18 17:41:45 +0900

    profile is now on feed

--- components/profile-list.tsx
+++ components/profile-list.tsx
@@ -1,16 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { profile } from 'console'
 import ReactMarkdown from 'react-markdown'
 
 function Ul(props) {
   return <ul className="ml-4 my-2 list-disc">{props.children}</ul>
 }
 
-export function Profile({ label, date, iconStyle, iconName }) {
+export function Profile({slug, label, date, iconStyle, iconName }) {
   const parsedLabel = (
     <ReactMarkdown children={label} renderers={{ list: Ul }} />
   )
   return (
-    <div className="bg-white rounded-xl shadow-xl p-6">
+    <div id={slug} className="bg-white rounded-xl shadow-xl p-6">
       <div className="mb-2"><FontAwesomeIcon className="w-5 mr-2 mb-1 inline" icon={[iconStyle, iconName]} />{date}</div>
       <div>{parsedLabel}</div>
     </div>
@@ -22,7 +23,8 @@ export function ProfileList({ profiles }) {
     <div className="z-10 grid grid-row-1 gap-y-6">
       {profiles.map((profile) => (
         <Profile
-          key={profile.slug}
+          key={profile.id}
+          slug={profile.slug}
           label={profile.label}
           iconStyle={profile.iconStyle}
           iconName={profile.iconName}
