31db1d29116c853a1a2dec29671017b1c15ab88f
Ryo Ando <sasigume@gmail.com>
2021-01-12 22:07:13 +0900

    removed twitter link

--- src/libs/icon.ts
+++ src/libs/icon.ts
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGithub, faTwitter, faFacebook, faHtml5, faCss3Alt, faGitAlt, faWordpress, faJsSquare, faReact} from '@fortawesome/free-brands-svg-icons'
-import { faExternalLinkAlt, faBook, faBorderAll, faUser, faDizzy, faSmile, faGrinBeam, faGrinHearts, faCheckSquare, faImages, faFilm} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faHtml5, faCss3Alt, faGitAlt, faWordpress, faJsSquare, faReact, faYoutube} from '@fortawesome/free-brands-svg-icons'
+import { faExternalLinkAlt, faPen, faBorderAll, faUser, faDizzy, faSmile, faGrinBeam, faGrinHearts, faCheckSquare, faImages, faFilm} from '@fortawesome/free-solid-svg-icons'
 
 export default function addIcon() {
-  library.add(faGithub, faTwitter, faFacebook, faHtml5, faCss3Alt, faGitAlt, faWordpress, faJsSquare, faReact, faExternalLinkAlt, faBook, faBorderAll, faUser, faDizzy, faSmile, faGrinBeam, faGrinHearts, faCheckSquare, faImages, faFilm)
+  library.add(faGithub, faHtml5, faCss3Alt, faGitAlt, faWordpress, faJsSquare, faReact, faYoutube, faExternalLinkAlt, faPen, faBorderAll, faUser, faDizzy, faSmile, faGrinBeam, faGrinHearts, faCheckSquare, faImages, faFilm)
 }
