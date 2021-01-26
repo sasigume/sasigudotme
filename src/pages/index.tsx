a36d98d6570c91777afd73ae51514960dafda2cd
Ryo Ando <sasigume@gmail.com>
2021-01-26 17:43:24 +0900

    removed ranking function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,48 +38,7 @@ export default function Home({
   )
 }
 
-// Using Jikan API https://jikan.docs.apiary.io/
-
-function convertAnime(anime, i) {
-  return [
-    { h2: i+1 + '. ' + anime.title + ' (' + anime.members + '人視聴)' ?? 'アニメタイトル' },
-    {
-      ul: [
-        'スコア: ' + anime.score ?? '10.0',
-        '放送開始時期: ' + anime.start_date ?? '2999-1-1',
-      ]
-    },
-    { p : ''}
-  ]
-}
-
 export const getStaticProps = async () => {
-  // Post anime ranking to my blog
-  const today = new Date().toLocaleDateString('ja')
-  const now = Date.now()
-  const firstText = `このランキングは、MyAnimeListの非公式API「Jikan」を使用し、毎日自動で生成しています。詳しくは以下のソースコードをご覧ください。\n\n[https://github.com/and0ry0/andoryocom/ :embed:cite]\n\nなお、デプロイにVercelを、送信にSendGridを使っているので、それぞれに何らかの障害が発生した場合は投稿されません。なお視聴者数は、**まだ見終わってない人も含みます**。\n\n`
-
-  const res = await fetch('https://api.jikan.moe/v3/top/anime/1/bypopularity')
-  const data = await res.json()
-  const topAnimes = data.top.map((anime, i) => convertAnime(anime, i)) ?? []
-  const firstAnime = {
-    title: data.top[0].title,
-    member: data.top[0].members
-  }
-  const json2md = require('json2md')
-  const text = json2md(topAnimes)
-
-  const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to: process.env.HATENABLOG_POST_EMAIL,
-    from: process.env.SENDER_EMAIL,
-    subject: today + '時点の世界アニメ人気ランキングTop50。1位は' + firstAnime.title + '(' + firstAnime.member + '人視聴) 生成時間:' + now,
-    text: firstText + text,
-  };
-  sgMail.send(msg);
-  console.log('Mail sended!')
-
   // skills and math questions
   const skillApi = new SkillApi()
   const allSkills = (await skillApi.fetchSkillEntries()) ?? []
