92e99eaacfa924affbdf546def24ecba9066c667
Ryo Ando <sasigume@gmail.com>
2021-01-26 10:45:59 +0900

    added sendgrid env variable

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ module.exports = {
         CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
         CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
         CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-        CONTENTFUL_PREVIEW_SECRET: process.env.CONTENTFUL_PREVIEW_SECRET
+        CONTENTFUL_PREVIEW_SECRET: process.env.CONTENTFUL_PREVIEW_SECRET,
+        SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
     },
     images: {
         // This is required to show images with <Image /> component
