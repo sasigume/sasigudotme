dacf04e725376128ab3f59c26f85bb33b2f0eee2
Ryo Ando <sasigume@gmail.com>
2021-02-01 22:04:37 +0900

    added character

--- src/options/constants.ts
+++ src/options/constants.ts
@@ -12,7 +12,7 @@ export const CONST_REPO_URL = 'https://github.com/and0ry0/andoryocom'
 export const CONST_YOUTUBE_URL = 'https://www.youtube.com/channel/UCBVFh_pGrysolohOzXMGqIA'
 
 export const CONST_OG_IMAGE_URL = 'https://og-image.vercel.app/**andoryo**.com.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg&images=https%3A%2F%2Fd21buns5ku92am.cloudfront.net%2F41748%2Fimages%2F347966-contentful-logo-wordmark-dark%2520%25281%2529-4cd185-large-1582664935.png&widths=350&widths=auto&heights=undefined&heights=auto'
-
+export const CONST_CHARACTER_IMAGE = 'https://images.ctfassets.net/8gzq2ae963cc/17OBMNMX3fFZ0NbbEmasCN/5cc8e6ed238f1ed0c73b7af9d19c72e6/render.png?h=800'
 
 export const CONST_LEVELS = [
   {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,70 @@
-import { CONST_LEVELS } from '../options/constants'
+import { CONST_LEVELS, CONST_CHARACTER_IMAGE} from '../options/constants'
 import { SkillApi, Skill } from '../services'
 import { ReactElement } from 'react'
-
-import { CONST_SITE_NAME } from '../options/constants'
-
-import Head from 'next/head'
+import Image from 'next/image'
 
 import { SkillMenu } from '../components/skill-list'
 import Layout from '../components/layout'
 
 type HomeProps = {
   preview: boolean,
-  allSkills: Skill[]
+  allSkills: Skill[],
+  sortedSkills: {
+    [key: string]: Skill[]
+  }
 }
 
 export default function Home({
   preview,
   allSkills,
+  sortedSkills
 }: HomeProps): ReactElement {
-  const weapon1 = allSkills.filter(function (skill) {return skill.slot == 'weapon1';}) ?? []
-  const weapon2 = allSkills.filter(function (skill) {return skill.slot == 'weapon2';}) ?? []
-  const weapon3 = allSkills.filter(function (skill) {return skill.slot == 'weapon3';}) ?? []
-  const weapon4 = allSkills.filter(function (skill) {return skill.slot == 'weapon4';}) ?? []
-  const head = allSkills.filter(function (skill) {return skill.slot == 'head';}) ?? []
-  const arm = allSkills.filter(function (skill) {return skill.slot == 'arm';}) ?? []
-  const body = allSkills.filter(function (skill) {return skill.slot == 'body';}) ?? []
-  const leg = allSkills.filter(function (skill) {return skill.slot == 'leg';}) ?? []
 
   return (
     <Layout page="index" preview={false}>
-      <SkillMenu direction="bottom" buttons={allSkills} />
+      <div className="flex items-center justify-around">
+        <div className="">
+          <SkillMenu direction="bottom" buttons={sortedSkills.weapon1 ? sortedSkills.weapon1.slice(0, 1) : []} />
+          <SkillMenu direction="bottom" buttons={sortedSkills.weapon2 ? sortedSkills.weapon2.slice(0, 1) : []} />
+          <SkillMenu direction="bottom" buttons={sortedSkills.weapon3 ? sortedSkills.weapon3.slice(0, 1) : []} />
+          <SkillMenu direction="bottom" buttons={sortedSkills.weapon4 ? sortedSkills.weapon4.slice(0, 1) : []} />
+        </div>
+        <div className="w-64">
+          <Image layout="responsive" width={500} height={800} src={CONST_CHARACTER_IMAGE} className="" />
+        </div>
+      </div>
       <br />
       <div className="my-6 mb-2 font-bold">Each color indicates skill level.</div>
-      <SkillMenu direction="top" buttons={CONST_LEVELS} />
+      <SkillMenu direction="bottom" buttons={CONST_LEVELS} />
     </Layout>
   )
 }
 
 export const getStaticProps = async () => {
+
   // skills and math questions
   const skillApi = new SkillApi()
   const allSkills = (await skillApi.fetchSkillEntries()) ?? []
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+  function groupBy(objectArray, property) {
+    return objectArray.reduce(function (acc, obj) {
+      let key = obj[property]
+      if (!acc[key]) {
+        acc[key] = []
+      }
+      acc[key].push(obj)
+      return acc
+    }, {})
+  }
+
+  // group by slots
+  const sortedSkills = groupBy(allSkills, 'slot')
+
   return {
     props: {
-      allSkills
+      allSkills,
+      sortedSkills
     },
   };
 }
